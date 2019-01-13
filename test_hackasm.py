0490b2c368494eacfa35cde781d0ed6887c1f883
Ruslan Forostianov <ruslan@thehyve.nl>
2019-01-13 13:47:15 +0100

    Add support for all C commands where a=0

--- test_hackasm.py
+++ test_hackasm.py
@@ -27,6 +27,70 @@ class HackAsmTestCase(unittest.TestCase):
         self._test_c_command_comp(comp='1', a='0', code='111111')
 
 
+    def test_minus_one_c_command(self):
+        self._test_c_command_comp(comp='-1', a='0', code='111010')
+
+
+    def test_d_c_command(self):
+        self._test_c_command_comp(comp='D', a='0', code='001100')
+
+
+    def test_a_c_command(self):
+        self._test_c_command_comp(comp='A', a='0', code='110000')
+
+
+    def test_not_d_c_command(self):
+        self._test_c_command_comp(comp='!D', a='0', code='001101')
+
+
+    def test_not_a_c_command(self):
+        self._test_c_command_comp(comp='!A', a='0', code='110001')
+
+
+    def test_minus_d_c_command(self):
+        self._test_c_command_comp(comp='-D', a='0', code='001111')
+
+
+    def test_minus_a_c_command(self):
+        self._test_c_command_comp(comp='-A', a='0', code='110011')
+
+
+    def test_d_plus_one_c_command(self):
+        self._test_c_command_comp(comp='D+1', a='0', code='011111')
+
+
+    def test_a_plus_one_c_command(self):
+        self._test_c_command_comp(comp='A+1', a='0', code='110111')
+
+
+    def test_d_minus_one_c_command(self):
+        self._test_c_command_comp(comp='D-1', a='0', code='001110')
+
+
+    def test_a_minus_one_command(self):
+        self._test_c_command_comp(comp='A-1', a='0', code='110010')
+
+
+    def test_d_plus_a_c_command(self):
+        self._test_c_command_comp(comp='D+A', a='0', code='000010')
+
+
+    def test_d_minus_a_c_command(self):
+        self._test_c_command_comp(comp='D-A', a='0', code='010011')
+
+
+    def test_a_minus_d_c_command(self):
+        self._test_c_command_comp(comp='A-D', a='0', code='000111')
+
+
+    def test_d_and_a_c_command(self):
+        self._test_c_command_comp(comp='D&A', a='0', code='000000')
+
+
+    def test_d_or_a_c_command(self):
+        self._test_c_command_comp(comp='D|A', a='0', code='010101')
+
+
     def _test_c_command_comp(self, comp, a, code):
         actual_code = hackasm.assemble(comp)
         self.assertEqual(actual_code, '111' + a + code + '000' + '000')
